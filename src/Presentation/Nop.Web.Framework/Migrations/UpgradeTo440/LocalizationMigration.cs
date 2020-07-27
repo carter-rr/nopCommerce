79b32a21dc97659a9bec4e198c8bcf84a527e0cf
DmitriyKulagin <kulagin87@gmail.com>
2020-07-27 10:25:07 +0300

    Added: - Setting enable MFA - New view for MFA plugins managment - Permission - MFAPluginManager - MFAType - MFAMethod - New widget zones

--- src/Presentation/Nop.Web.Framework/Migrations/UpgradeTo440/LocalizationMigration.cs
+++ src/Presentation/Nop.Web.Framework/Migrations/UpgradeTo440/LocalizationMigration.cs
@@ -108,7 +108,19 @@ namespace Nop.Web.Framework.Migrations.UpgradeTo440
                 "RewardPoints.Message.Expired",
                 "ShoppingCart.AddToWishlist.Update",
                 "ShoppingCart.UpdateCartItem",
-                "Tax.SelectType"
+                "Tax.SelectType",
+
+                //<MFA #475>
+                "Admin.Configuration.ExternalAuthenticationMethods",
+                "Admin.Configuration.ExternalAuthenticationMethods.BackToList",
+                "Admin.Configuration.ExternalAuthenticationMethods.Configure",
+                "Admin.Configuration.ExternalAuthenticationMethods.Fields.DisplayOrder",
+                "Admin.Configuration.ExternalAuthenticationMethods.Fields.FriendlyName",
+                "Admin.Configuration.ExternalAuthenticationMethods.Fields.IsActive",
+                "Admin.Configuration.ExternalAuthenticationMethods.Fields.SystemName",
+                "Permission.ManageExternalAuthenticationMethods"
+                //</MFA #475>
+
             });
 
             localizationService.AddLocaleResource(new Dictionary<string, string>
@@ -214,7 +226,32 @@ namespace Nop.Web.Framework.Migrations.UpgradeTo440
                 ["Admin.Configuration.Shipping.Providers.DownloadMorePlugins"] = "You can download more plugins in our <a href=\"https://www.nopcommerce.com/extensions?category=shipping-delivery&utm_source=admin-panel&utm_medium=menu&utm_campaign=marketplace&utm_content=shipping-plugins\" target=\"_blank\">marketplace</a>",
                 ["Admin.Configuration.Tax.Providers.DownloadMorePlugins"] = "You can download more plugins in our <a href=\"https://www.nopcommerce.com/extensions?category=taxes&utm_source=admin-panel&utm_medium=menu&utm_campaign=marketplace&utm_content=tax-plugins\" target=\"_blank\">marketplace</a>",
                 ["Admin.Configuration.Settings.GeneralCommon.DefaultStoreTheme.GetMore"] = "You can get more themes in our <a href=\"https://www.nopcommerce.com/themes?utm_source=admin-panel&utm_medium=menu&utm_campaign=marketplace&utm_content=general-common-theme\" target=\"_blank\">marketplace</a>",
-                ["Admin.Configuration.Plugins.OfficialFeed.Instructions"] = "Here you can find third-party extensions and themes which are developed by our community and partners. They are also available in our <a href=\"https://www.nopcommerce.com/marketplace?utm_source=admin-panel&utm_medium=menu&utm_campaign=marketplace&utm_content=official-plugins\" target=\"_blank\">marketplace</a>"
+                ["Admin.Configuration.Plugins.OfficialFeed.Instructions"] = "Here you can find third-party extensions and themes which are developed by our community and partners. They are also available in our <a href=\"https://www.nopcommerce.com/marketplace?utm_source=admin-panel&utm_medium=menu&utm_campaign=marketplace&utm_content=official-plugins\" target=\"_blank\">marketplace</a>",
+                ["Admin.System.Warnings.PluginNotEnabled.AutoFixAndRestart"] = "Uninstall and delete all not used plugins automatically (site will be restarted)",
+                //<MFA #475>
+                ["Admin.Configuration.Authentication"] = "Authentication",
+                ["Admin.Configuration.Authentication.ExternalMethods"] = "External authentication",
+                ["Admin.Configuration.Authentication.ExternalMethods.BackToList"] = "back to external authentication method list",
+                ["Admin.Configuration.Authentication.ExternalMethods.Configure"] = "Configure",
+                ["Admin.Configuration.Authentication.ExternalMethods.Fields.DisplayOrder"] = "Display order",
+                ["Admin.Configuration.Authentication.ExternalMethods.Fields.FriendlyName"] = "Friendly name",
+                ["Admin.Configuration.Authentication.ExternalMethods.Fields.IsActive"] = "Is active",
+                ["Admin.Configuration.Authentication.ExternalMethods.Fields.SystemName"] = "System name",
+                ["Admin.Configuration.Authentication.MultiFactorMethods"] = "Multifactor authentication",
+                ["Admin.Configuration.Authentication.MultiFactorMethods.BackToList"] = "back to multifactor authentication method list",
+                ["Admin.Configuration.Authentication.MultiFactorMethods.Configure"] = "Configure",
+                ["Admin.Configuration.Authentication.MultiFactorMethods.Fields.DisplayOrder"] = "Display order",
+                ["Admin.Configuration.Authentication.MultiFactorMethods.Fields.FriendlyName"] = "Friendly name",
+                ["Admin.Configuration.Authentication.MultiFactorMethods.Fields.IsActive"] = "Is active",
+                ["Admin.Configuration.Authentication.MultiFactorMethods.Fields.SystemName"] = "System name",
+
+                ["Permission.Authentication.ManageExternalMethods"] = "Admin area. Manage External Authentication Methods",
+                ["Permission.Authentication.ManageMultifactorMethods"] = "Admin area. Manage Multifactor Authentication Methods",
+
+                ["Admin.Configuration.Settings.CustomerUser.EnableMultifactorAuth"] = "Enable multifactor authentication",
+                ["Admin.Configuration.Settings.CustomerUser.EnableMultifactorAuth.Hint"] = "When enabled, users attempting to access the public store must complete a second step to verify their account."
+                //</MFA #475>
+
             });
 
             // rename locales
